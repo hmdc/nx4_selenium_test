14be4226274df95acbde898d17a54fe8a74a5b21
Evan Sarmiento <esarmien@g.harvard.edu>
2014-09-16 15:33:38 -0400

    /usr/local/bin/python scripts/nx4_stress.py -l http://dev-rce.hmdc.harvard.edu -u "`echo rcetest{1..10}`" -p s3l3n1um -i 5

--- scripts/nx4_stress.py
+++ scripts/nx4_stress.py
@@ -1,31 +1,35 @@
 #!/usr/local/bin/python
 from nx4_selenium_test import NX4WebClient
 from time import sleep
+import signal
 import multiprocessing
+import argparse
 
-_GLOBAL_TIMEOUT = 30
-
-def test_new_session_loop(uri, user, passwd):
-  client = NX4WebClient(uri, user, passwd)
+def test_new_session_loop(client, uri, user, passwd, iterations=10):
   # We want this to go on forever, we know it's gonna mess up sometimes.
-  while 1:
+  
+  for i in range(0, iterations):
     try:
+      print "[" + str(i) + "]" + " Starting test_new_session_loop()"
       client.test_new_session()
     except:
       pass
 
-def test_resume_session_loop(uri, user, passwd):
-  client = NX4WebClient(uri, user, passwd)
-  while 1:
+def test_resume_session_loop(client, uri, user, passwd, iterations=10):
+
+  for i in range (0, iterations):
     try:
+      print "[" + str(i) + "]" + " Starting test_resume_session_loop()"
       client.test_resume_session()
     except:
       pass
 
-def test_new_resume_delay_loop(uri, user, passwd):
-  client = NX4WebClient(uri, user, passwd)
+
+
+def test_new_resume_delay_loop(client, uri, user, passwd, iterations=10):
   while 1:
     try:
+      print "[" + str(i) + "]" + " Starting test_new_resume_delay_loop()"
       client.test_new_session()
       sleep(20)
       client.test_resume_session()
@@ -39,27 +43,69 @@ def nx4_worker(info):
   uri = info['uri']
   user = info['user']
   passwd = info['passwd']
+  client = NX4WebClient(uri, user, passwd)
 
-  test(uri, user, passwd)
+  try:
+    _iterations = info['iterations']
+  except:
+    _iterations = None
+
+  if _iterations:
+    test(client, uri, user, passwd, iterations=_iterations)
+  else:
+    test(client, uri, user, passwd)
+
+  client.quit()
+
 
 # number of iterations
 # processes, and username list must be equal
 # username list
 
 if __name__ == '__main__':
+
+  parser = argparse.ArgumentParser(description="Stress test NX4 web\
+      interface")
+
+  parser.add_argument('-d', '--debug', action='store_true',
+                      help="Enables verbose output.")
+  parser.add_argument('-l', '--location', required=True,
+                      help="URI of NoMachine NX4 web server")
+  parser.add_argument('-u', '--user', required=True,
+                      help="Usernames separated by a space")
+  parser.add_argument('-p', '--passwd', required=True,
+                      help="Username password")
+  parser.add_argument('-i', '--iterations', type=True,
+                      help="Number of iterations")
+
+  args = parser.parse_args()
+
+  _users = args.user.split(" ")
+  _passwd = args.passwd
+  _uri = args.location
+  _num_processes = len(_users)
+  _iterations = args.iterations
+
   lp = []
-  for i in range(1, 6):
-    user = "rcetest" + str(i)
+
+  for usr in _users:
     lp_obj = {
         'test': test_new_session_loop,
-        'uri': 'http://dev-rce.hmdc.harvard.edu',
-        'user': user,
-        'passwd': 'XYZ',
-    }
+        'uri': _uri,
+        'user': usr,
+        'passwd': _passwd,
+        'iterations': _iterations
+        }
     lp.append(lp_obj)
 
-  pool = multiprocessing.Pool(processes=5)
-  pool_outputs = pool.map(nx4_worker, lp)
-  pool.close()
-  pool.join()
 
+  pool = multiprocessing.Pool(processes=_num_processes)
+  
+  try:
+    pool.map_async(nx4_worker, lp)
+    pool.close()
+    pool.join()
+  except KeyboardInterrupt:
+    print "Caught Ctrl+C, terminating workers."
+    pool.terminate()
+    pool.join()
